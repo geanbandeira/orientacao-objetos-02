774a70118fe9cb4c8f8b9dc4683b8922af208bb8
geanbandeira <60990296+geanbandeira@users.noreply.github.com>
2020-03-10 15:29:55 -0300

    Update Circulo.java

--- src/main/java/exercicios/Circulo.java
+++ src/main/java/exercicios/Circulo.java
@@ -1,1 +1,26 @@
+package exercicios;
+public class Circulo {
 
+    public Double raio;
+    private Double PI = 3.1415;
+    public Circulo(){
+}
+    public Double getPI(){
+    return PI;
+}
+
+    public Double getRaio() {
+    return raio;
+}
+
+    public void setRaio(Double raio) {
+      this.raio = raio;
+}
+    public void calcularArea(Double raio){
+      this.raio = this.PI * raio * raio;
+}
+    public void calcularPerimentro(Double raio){
+      this.raio = 2 * (this.PI * raio);
+}
+
+}
