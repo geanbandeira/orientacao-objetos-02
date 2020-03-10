ea35c6173c7999d2432c0b7232864df1bc1728a3
geanbandeira <60990296+geanbandeira@users.noreply.github.com>
2020-03-10 15:45:12 -0300

    Update CirculoTeste.java

--- src/test/java/exercicios/CirculoTeste.java
+++ src/test/java/exercicios/CirculoTeste.java
@@ -1,1 +1,35 @@
+package exercicios;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class CorrentistaTeste {
+    private Correntista co1;
+    @BeforeEach
+    public void beforeEach(){
+        Correntista con1 = new Correntista();
+        co1.codigo ="1";
+        co1.nome = "Carlos";
+        co1.email = "Carlos@gg.com";
+        co1.telefone ="111111111111";
+    }
+  
+    @AfterEach
+    public void afterEach(){
+        co1=null;
+    }
+
+    @Test
+    public void criarObjetoCorrentista(){
+        assertNotNull(co1);
+    }
+  
+    @Test
+    public void criarAtributos(){
+        assertNotNull(co1.getCodigoCliente());
+    }
+}
