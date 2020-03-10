4779623fa4ab771b7237b2459c50fec8a3c69a84
geanbandeira <60990296+geanbandeira@users.noreply.github.com>
2020-03-10 15:33:29 -0300

    Update Retangulo.java

--- src/main/java/exercicios/Retangulo.java
+++ src/main/java/exercicios/Retangulo.java
@@ -1,1 +1,29 @@
+package exercicios;
 
+public class Retangulo {
+    public Double base;
+    public Double altura;
+
+    public Retangulo(){
+    }
+        public Double getBase(){
+            return base;
+        }
+        public void setBase(Double base){
+            this.base = base;
+        }
+
+        public Double getAltura(){
+            return altura;
+        }
+
+        public void setAltura(Double altura){
+            this.altura = altura;
+        }
+            public void calcularArea(Double area){
+                area = base*altura;
+            }
+                public void calcularPerimentro(Double perimetro){
+                    perimetro = 2*(base + altura);
+                }
+}
