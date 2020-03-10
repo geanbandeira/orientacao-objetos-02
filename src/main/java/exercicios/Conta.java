1a343f934f5a830f8ce8dc4029426b4eccc74c87
geanbandeira <60990296+geanbandeira@users.noreply.github.com>
2020-03-10 15:41:10 -0300

    Update Conta.java

--- src/main/java/exercicios/Conta.java
+++ src/main/java/exercicios/Conta.java
@@ -1,1 +1,7 @@
+package exercicios;
 
+public class Conta {
+    public Integer numero;
+    public Double saldo;                            //Continuar depois adicionando o solicitado no exercicio.
+    public Double limite;
+}
