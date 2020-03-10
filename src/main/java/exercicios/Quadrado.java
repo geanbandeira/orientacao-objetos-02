db1533b768d3c5b100c7dc951c1021129dac8ee6
geanbandeira <60990296+geanbandeira@users.noreply.github.com>
2020-03-10 15:35:31 -0300

    Update Quadrado.java

--- src/main/java/exercicios/Quadrado.java
+++ src/main/java/exercicios/Quadrado.java
@@ -1,1 +1,22 @@
+package exercicios;
 
+public class Quadrado{
+    public Double lado;
+    public Quadrado(){
+    }
+    public Quadrado(Double lado){
+        this.lado = lado;
+    }
+        public Double getLado(){
+            return lado;
+        }
+            public void setLado(Double lado){
+                this.lado = lado;
+            }
+                public void calcularArea(Double lado){
+                    this.lado = lado * lado;
+                }
+                    public void calcularPerimetro(Double lado){
+                        this.lado = 4 * lado;
+                    }
+}
